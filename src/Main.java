08f6a5a4800ad4ad723dacd09f30acfbcf42bb99
pablo <pablomg01@hotmail.com>
2023-07-11 22:32:25 +0200

    codigo4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! cooomoooo fffff   gggogoogoog  sa cambiado");
+        System.out.println("Hello world! cooomoooo fffff   gggogoogoog  sa cambiado otrooooo");
     }
 }
